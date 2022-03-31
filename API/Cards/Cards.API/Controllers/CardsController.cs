939c136638a114c5e64b153f96d7565104627194
Miro Mac <itcodevelopment@gmail.com>
2022-03-31 16:52:39 +0300

    EntityFramework setup

---
+++ API/Cards/Cards.API/Controllers/CardsController.cs
@@ -0,0 +1,26 @@
+﻿using Cards.API.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cards.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CardsController : Controller
+    {
+        private readonly CardsDbContext cardsDbContext;
+
+        public CardsController(CardsDbContext cardsDbContext)
+        {
+            this.cardsDbContext = cardsDbContext;
+        }
+
+        // Get All Cards
+        [HttpGet]
+        public async Task<IActionResult> GetAllCards()
+        {
+            var cards = await cardsDbContext.Cards.ToListAsync();
+            return Ok(cards);
+        }
+    }
+}
